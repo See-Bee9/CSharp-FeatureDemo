de2780e22e5710388e45c02dc0b904317680b404
Colin Bird <Colin.Bird@ClaytonHomes.com>
2024-02-22 15:14:10 -0500

    Implemented spread

--- Features/Spread.cs
+++ Features/Spread.cs
@@ -1,7 +1,31 @@
 ﻿namespace Feature_Demo.Features
 {
-    public class Spread
+    public class Spread : IDemonstrable
     {
-        
+        private void Print(TextWriter writer, string prefix, int[] array)
+        {
+            var arrayString = String.Join(", ", array);
+
+            writer.WriteLine($"{prefix} : {arrayString}");
+        }
+
+        public void Demo(TextWriter outStream)
+        {
+            int[] first = [1, 2, 3];
+            int[] second = [4, 5, 6];
+            int[] third = [7, 8, 9];
+
+            int[] flat = [..first, ..second, ..third];
+            Print(outStream, "Flattened", flat);
+
+            var first5 = flat[5..];
+            Print(outStream, "First five", first5);
+
+            var last5 = flat[..5];
+            Print(outStream, "Last five", last5);
+
+            var middle = flat[3..5];
+            Print(outStream, "Middle", middle);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Feature_Demo.Features;
+
+IDemonstrable demonstrable = new Spread();
+
+demonstrable.Demo(Console.Out);
